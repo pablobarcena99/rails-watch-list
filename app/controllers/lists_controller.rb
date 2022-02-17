c41d2c01421442f4bce7cc9ca1b722db65c9d7ce
pablo barcena <pablobarcena99@gmail.com>
2022-02-17 19:56:44 +0100

    added sccs

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,26 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = @list.bookmarks
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to lists_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :url)
+  end
 end
