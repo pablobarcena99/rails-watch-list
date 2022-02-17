a10cddd823d61f2ac3107986433ce52504ae1065
pablo barcena <pablobarcena99@gmail.com>
2022-02-17 12:08:39 +0100

    added models controller and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+  url = "http://tmdb.lewagon.com/movie/top_rated"
+  user_serialized = URI.open(url).read
+  user = JSON.parse(user_serialized)
+
+  results_array = user["results"]
+
+  results_array.each do |film|
+    Movie.create(title: film["title"], overview: film["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{film["poster_path"]}", rating: film["vote_average"])
+  end
